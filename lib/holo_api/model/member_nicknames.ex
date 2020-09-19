797c8b0db89313a3234d75e1eb015c7f180e3381
Jannis Becker <jbecker@jannisbecker.me>
2020-09-19 18:35:39 +0200

    Fix member_nicknames schema

--- lib/holo_api/model/member_nicknames.ex
+++ lib/holo_api/model/member_nicknames.ex
@@ -1,8 +1,12 @@
 defmodule HoloApi.Model.MemberNickname do
   use Ecto.Schema
 
+  alias HoloApi.Model.Member
+
   schema "member_nicknames" do
     field :name, :string
+
+    belongs_to :member, Member
 
     timestamps()
   end
