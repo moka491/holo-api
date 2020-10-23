10cb0ad813cd28d8da3a935ec33ccad9395f089a
Jannis <jbecker@jannisbecker.me>
2020-10-23 18:30:34 +0200

    Add more seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,6 +1,37 @@
 alias HoloApi.Repo
 alias HoloApi.Model.Company
 alias HoloApi.Model.Agency
+alias HoloApi.Model.Member
+alias HoloApi.Model.MemberNickname
 
-cover = %Company{name: "Cover Corp", name_jp: "カバー", website_url: "https://cover-corp.com/"} |> Repo.insert!
-hololive = %Agency{name: "Hololive Production", name_jp: "ホロライブプロダクション", website_url: "https://www.hololive.tv/", company_id: cover.id} |> Repo.insert!
+cover =
+  %Company{name: "Cover Corp", name_jp: "カバー", website_url: "https://cover-corp.com/"}
+  |> Repo.insert!()
+
+hololive =
+  %Agency{
+    name: "Hololive Production",
+    name_jp: "ホロライブプロダクション",
+    website_url: "https://www.hololive.tv/",
+    company_id: cover.id
+  }
+  |> Repo.insert!()
+
+# agency_id: hololive.id, }
+subaru =
+  %Member{
+    name: "Oozora Subaru",
+    name_jp: "大空スバル",
+    debut_date: DateTime.from_unix!(0),
+    height: 154,
+    gender: "Female",
+    zodiac: "Cancer",
+    emoji: "🚑",
+    nicknames: [
+      %MemberNickname{nickname: "Subaru"},
+      %MemberNickname{nickname: "Subachi"},
+      %MemberNickname{nickname: "Subatani Subao"}
+    ],
+    agency_id: hololive.id
+  }
+  |> Repo.insert!()
